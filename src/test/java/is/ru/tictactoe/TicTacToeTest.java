dcee76c5df84f1ceaddf5dccd7c1c24f682a6b9f
Sigurdur Mar Atlason <siggi@i-8eae37da.greenqloud.internal>
2014-11-02 01:10:40 +0000

    Initial commit, project ready to work on

---
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -0,0 +1,18 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TicTacToeTest {
+
+	public static void main(String args[]) {
+      org.junit.runner.JUnitCore.main("is.ru.tictactoe.TicTacToeTest");
+    }
+	
+	/*
+	@Test
+	public void testNumberOne() {
+		assertEquals(expected, test());
+	}
+	*/
+}
